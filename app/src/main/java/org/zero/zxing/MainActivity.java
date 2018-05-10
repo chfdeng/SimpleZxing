ee5a302d24d801f5ceb31ba266daa86d30601bcd
fengchuiyeluo <chfdeng@gmail.com>
2018-05-10 10:08:52 +0800

    更改项目结构

--- app/src/main/java/org/zero/zxing/MainActivity.java
+++ app/src/main/java/org/zero/zxing/MainActivity.java
@@ -1,12 +1,20 @@
 package org.zero.zxing;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
 import com.google.zxing.WriterException;
+import com.zero.libzxing.ZxingUtil;
+import com.zero.libzxing.core.DecodeActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,7 +23,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final ImageView image = (ImageView) findViewById(R.id.imageView);
-
         findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -31,8 +38,29 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ZxingUtil.getInstance().decode(MainActivity.this);
+                if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                    ZxingUtil.getInstance().decode(MainActivity.this);
+                } else {
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA}, 1000);
+                }
             }
         });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 1000 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            ZxingUtil.getInstance().decode(MainActivity.this);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == DecodeActivity.REQUEST_CODE_DECODE && requestCode == RESULT_OK) {
+            String stringExtra = data.getStringExtra(DecodeActivity.DECODE_RESULT);
+            Log.e("", "结果：" + stringExtra);
+        }
     }
 }
